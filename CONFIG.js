644ae65a9d81369a0dd73f3a2fff0ba5dfe5d24a
louki-vn <hieu7apro@gmail.com>
2025-05-20 19:21:28 +0700

    .

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,11 @@
 const CONFIGDATA = {
-  titleHeader: "Chào em, anh có điều muốn nói. </br> Ngày kia đi chơi với anh rồi cho anh hỗn nha!",
+  titleHeader:
+    'Hi ny, anh có điều muốn nói </br> Anh dự định sẽ đi Huế chơi vào cuối tháng này (Dự kiến 30/5 - 01/6) </br> Em có muốn đi chơi xa cùng với anh không :))',
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu em im lặng và rời đi thì tức là em đã ĐỒNG Ý.",
-  buttonYes: "Em đồng ý",
-  buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn Em.",
+    'Hãy trả lời một cách thật lòng nhé. Nếu em im lặng và rời đi thì tức là em đã ĐỒNG Ý.',
+  buttonYes: 'Em đồng ý',
+  buttonNo: 'Không đồng ý',
+  titleModar: 'Cảm ơn Em.',
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    'Cảm ơn em đã đồng ý và đồng hành cùng với anh. </br> Đi cùng nhau và tạo ra những kỷ niệm đáng nhớ nha.</br> Love you <3',
 };
