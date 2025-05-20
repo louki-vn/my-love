62bb61a2f68c8ae854f1f98bf39c64aac6eacaf7
louki-vn <hieu7apro@gmail.com>
2025-05-20 15:24:58 +0700

    first commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Chào em, anh có điều muốn nói. </br> Ngày kia đi chơi với anh rồi cho anh hỗn nha!",
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé. Nếu em im lặng và rời đi thì tức là em đã ĐỒNG Ý.",
+  buttonYes: "Em đồng ý",
+  buttonNo: "Không đồng ý",
+  titleModar: "Cảm ơn Em.",
+  descriptionModar:
+    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+};
