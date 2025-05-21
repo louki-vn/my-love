74ea5f33b51db35f6cbbde3cbd9147636f08fef2
louki-vn <hieu7apro@gmail.com>
2025-05-21 08:26:58 +0700

    .

--- CONFIG.js
+++ CONFIG.js
@@ -3,8 +3,8 @@ const CONFIGDATA = {
     'Chào người yêu dấu <3 </br> Anh dự định sẽ đi Huế chơi vào cuối tháng này từ 30/5 - 01/6 </br> Em có muốn đi chơi xa cùng với anh không',
   descriptionHeader:
     'Hãy trả lời một cách thật lòng nhé </br> Nếu em không muốn đi thì chọn không đồng ý nha :))',
-  buttonYes: 'Em đồng ý',
-  buttonNo: 'Không đồng ý',
+  buttonYes: 'Em đồng ý <3',
+  buttonNo: 'Không nhé :P',
   titleModar: 'Tuyệt vời ',
   descriptionModar:
     'Cảm ơn em đã đồng ý và đồng hành cùng với anh. </br> Hãy đi cùng nhau và tạo ra thật nhiều những kỷ niệm đáng nhớ nha.</br> Love you <3',
